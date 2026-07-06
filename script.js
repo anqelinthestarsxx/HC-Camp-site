c49e6ddb85418959eef086b87cb0871439026ba6
anqelinthestarsxx <luvdemixo@hotmail.com>
2026-07-05 22:11:12 -0400

    Implement greeting and time update functions
    Added functions to update greeting and time display.

---
+++ script.js
@@ -0,0 +1,34 @@
+function updateGreeting() {
+    const hour = new Date().getHours();
+    let greeting;
+
+    if (hour < 12) {
+        greeting = "Good morning!";
+    } else if (hour < 18) {
+        greeting = "Good afternoon!";
+    } else {
+        greeting = "Good evening!";
+    }
+
+    document.getElementById("greeting").textContent = greeting;
+}
+
+function updateTime() {
+    const now = new Date();
+
+    document.getElementById("time").textContent =
+        now.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit"
+        });
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+
+function backToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
